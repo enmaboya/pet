c63bb869e62e7a6d7740c7d7ab18c66a9397980c
Enmaboya <vita-dubov@yandex.ru>
2023-05-31 05:05:16 +0400

    init

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,23 @@
+import webpack from "webpack";
+
+export function buildLoaders(): webpack.RuleSetRule[] {
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+        ]
+    }
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader,
+    ]
+}
